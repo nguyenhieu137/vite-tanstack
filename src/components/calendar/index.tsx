13005ca81393845dc7e448a153e649039f75accb
jason.nguyen <jason.nguyen@techvify.com.vn>
2024-06-06 15:06:36 +0700

    add calendar/ add to homepage

---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "./index.scss";
+
+interface Event {
+  id: string;
+  title: string;
+  startRecur: Date;
+  endRecur?: Date;
+  daysOfWeek: number[];
+  color?: string;
+}
+
+interface CalendarComponentProps {
+  events: Event[];
+  initialView?: string;
+  weekends?: boolean;
+  height?: string;
+}
+
+const CalendarComponent: React.FC<CalendarComponentProps> = ({
+  events,
+  initialView = "dayGridMonth",
+  weekends = true,
+  height = "80vh",
+}) => {
+  function renderEventContent(eventInfo: {
+    event: { title: string; start: Date; end?: Date; backgroundColor?: string };
+  }) {
+    const { title, backgroundColor } = eventInfo.event;
+    //const { title, start, end, backgroundColor } = eventInfo.event;
+    // const startDate = new Date(start).toLocaleDateString();
+    // const startTime = new Date(start).toLocaleTimeString(undefined, {
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    // });
+    // let displayEndTime = "";
+    // let displayEndDate = "";
+
+    // if (end) {
+    //   const endDate = new Date(end).toLocaleDateString();
+    //   const endTime = new Date(end).toLocaleTimeString(undefined, {
+    //     hour: '2-digit',
+    //     minute: '2-digit',
+    //   });
+
+    //   if (startDate === endDate) {
+    //     displayEndTime = endTime;
+    //   } else {
+    //     displayEndDate = endDate;
+    //     displayEndTime = endTime;
+    //   }
+    // }
+
+    // const duration = end
+    //   ? `${Math.ceil((new Date(end).getTime() - new Date(start).getTime()) / (1000 * 60 * 60))} hours`
+    //   : "";
+
+    return (
+      <div
+        className="event flex flex-col h-[100%] w-full cursor-pointer"
+        style={{ backgroundColor }}
+      >
+        {/* <span>{startDate} {startTime}</span>
+        {displayEndDate && <span>{displayEndDate} {displayEndTime}</span>}
+        {!displayEndDate && displayEndTime && <span>{displayEndTime}</span>}
+        {duration && <span>{duration}</span>} */}
+        <span>{title}</span>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={initialView}
+        headerToolbar={{
+          start: "today prev,next",
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        height={height}
+        weekends={weekends}
+        events={events}
+        eventContent={renderEventContent}
+      />
+    </div>
+  );
+};
+
+export default CalendarComponent;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+import CalendarComponent from "@/components/calendar";
+// import { callMsGraph } from '@/services/graph';
+// import { loginRequest } from '@/services/request';
+// import { useMsal } from '@azure/msal-react';
+
+interface Event {
+  id: string;
+  title: string;
+  startRecur: Date; // Start recurrence date in ISO format
+  endRecur: Date; // End recurrence date in ISO format
+  // startTime: string; // Start time of the event
+  // endTime: string; // End time of the event
+  daysOfWeek: number[]; // Days of the week the event occurs (0=Sunday, 1=Monday, etc.)
+  color: string;
+}
+const events: Event[] = [
+  {
+    id: "1",
+    title: "All Day Event",
+    startRecur: new Date("2024-06-01T00:00:00"),
+    endRecur: new Date("2024-06-01T23:59:59"),
+    // startTime: '00:00:00',
+    // endTime: '23:59:59',
+    daysOfWeek: [6], // Saturday
+    color: "#32a852",
+  },
+  {
+    id: "2",
+    title: "Long Event",
+    startRecur: new Date("2024-06-07T09:00:00"),
+    endRecur: new Date("2024-06-10T17:00:00"),
+    // startTime: '09:00:00',
+    // endTime: '17:00:00',
+    daysOfWeek: [5, 6, 0], // Friday, Saturday, Sunday
+    color: "#2b20bd",
+  },
+  {
+    id: "3",
+    title: "Meeting",
+    startRecur: new Date("2024-06-03T10:30:00"),
+    endRecur: new Date("2024-06-10T12:30:00"),
+    // startTime: '10:30:00',
+    // endTime: '12:30:00',
+    daysOfWeek: [1, 2, 3, 4, 5], // Monday to Friday
+    color: "#bf2259",
+  },
+  {
+    id: "4",
+    title: "Meeting techvify D1",
+    startRecur: new Date("2024-06-08T10:30:00"),
+    endRecur: new Date("2024-06-08T12:30:00"),
+    // startTime: '10:30:00',
+    // endTime: '12:30:00',
+    daysOfWeek: [6], // Saturday
+    color: "#bf2251",
+  },
+  // Sự kiện lặp lại hàng ngày từ 6 đến 8 tháng 6
+  {
+    id: "5",
+    title: "Daily Meeting",
+    startRecur: new Date("2024-06-06T08:30:00"),
+    endRecur: new Date("2024-06-08T09:00:00"),
+    // startTime: '08:30:00',
+    // endTime: '09:00:00',
+    daysOfWeek: [4, 5, 6], // Thursday, Friday, Saturday
+    color: "#ffaa00",
+  },
+];
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container w-full flex justify-center">
+      {/* <button onClick={RequestProfileData}>get data</button> */}
+      <div className="calendar-container w-[90%] md:w-[60%]">
+        <CalendarComponent events={events} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
