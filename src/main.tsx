7bea8786b7a9e992186d863fff3f9bd0e82268b4
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-06-06 22:59:20 +0700

    Add tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from "./routeTree.gen.ts";
 const router = createRouter({ routeTree });
-
+const queryClient = new QueryClient();
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+       <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+       </QueryClientProvider>
     </StrictMode>,
   );
 }
