95f529148ef2b8ccc9dacbb37b12b8aaa3961293
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-06-06 23:01:08 +0700

    handle navigate

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -3,6 +3,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useRouter } from "@tanstack/react-router";
 import "./index.scss";
 
 interface Event {
@@ -26,11 +27,19 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({
   initialView = "dayGridMonth",
   weekends = true,
   height = "80vh",
-}) => {
-  function renderEventContent(eventInfo: {
-    event: { title: string; start: Date; end?: Date; backgroundColor?: string };
-  }) {
-    const { title, backgroundColor } = eventInfo.event;
+  }) => {
+    const router = useRouter();
+    
+    const handleDateClick = () => {
+      router.navigate({ to: "/meeting" });
+    };
+    const handleEventClick= () => {
+      router.navigate({ to: "/meeting/:id" });
+    };
+    function renderEventContent(eventInfo: {
+      event: { title: string; start: Date; end?: Date; backgroundColor?: string };
+      }) {
+        const { title, backgroundColor } = eventInfo.event;
     //const { title, start, end, backgroundColor } = eventInfo.event;
     // const startDate = new Date(start).toLocaleDateString();
     // const startTime = new Date(start).toLocaleTimeString(undefined, {
@@ -61,6 +70,7 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({
 
     return (
       <div
+        onClick={handleEventClick}
         className="event flex flex-col h-[100%] w-full cursor-pointer"
         style={{ backgroundColor }}
       >
@@ -87,6 +97,7 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({
         weekends={weekends}
         events={events}
         eventContent={renderEventContent}
+        dateClick={handleDateClick}
       />
     </div>
   );
