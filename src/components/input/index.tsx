ff48ba87a29439bcc630ca5bd810bc73248de0ab
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-06-06 23:01:24 +0700

    input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface InputFieldProps {
+  type: string;
+  name: string;
+  id: string;
+  label: string;
+  required?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ type, name, id, label, required = false, onChange, value }) => {
+  return (
+    <div className="relative z-0 w-full mb-5 group">
+      <input
+        type={type}
+        name={name}
+        id={id}
+        className="block py-2.5 px-0 w-full text-[16px] font-[500] cursor-pointer text-[#000000] bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=" "
+        required={required}
+        onChange={onChange}
+        value={value}
+      />
+      <label
+        htmlFor={id}
+        className="peer-focus:font-medium absolute text-[1rem] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default InputField;
