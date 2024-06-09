7536ac65ddbcc1280ca83a6a624d1876412a16db
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-06-09 13:20:51 +0700

    remove day of week

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -11,7 +11,6 @@ interface Event {
   title: string;
   startRecur: Date;
   endRecur?: Date;
-  daysOfWeek: number[];
   color?: string;
 }
 
@@ -27,46 +26,19 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({
   initialView = "dayGridMonth",
   weekends = true,
   height = "80vh",
-  }) => {
-    const router = useRouter();
-    
-    const handleDateClick = () => {
-      router.navigate({ to: "/meeting" });
-    };
-    const handleEventClick= () => {
-      router.navigate({ to: "/meeting/:id" });
-    };
-    function renderEventContent(eventInfo: {
-      event: { title: string; start: Date; end?: Date; backgroundColor?: string };
-      }) {
-        const { title, backgroundColor } = eventInfo.event;
-    //const { title, start, end, backgroundColor } = eventInfo.event;
-    // const startDate = new Date(start).toLocaleDateString();
-    // const startTime = new Date(start).toLocaleTimeString(undefined, {
-    //   hour: '2-digit',
-    //   minute: '2-digit',
-    // });
-    // let displayEndTime = "";
-    // let displayEndDate = "";
+}) => {
+  const router = useRouter();
 
-    // if (end) {
-    //   const endDate = new Date(end).toLocaleDateString();
-    //   const endTime = new Date(end).toLocaleTimeString(undefined, {
-    //     hour: '2-digit',
-    //     minute: '2-digit',
-    //   });
-
-    //   if (startDate === endDate) {
-    //     displayEndTime = endTime;
-    //   } else {
-    //     displayEndDate = endDate;
-    //     displayEndTime = endTime;
-    //   }
-    // }
-
-    // const duration = end
-    //   ? `${Math.ceil((new Date(end).getTime() - new Date(start).getTime()) / (1000 * 60 * 60))} hours`
-    //   : "";
+  const handleDateClick = () => {
+    router.navigate({ to: "/meeting" });
+  };
+  const handleEventClick = () => {
+    router.navigate({ to: "/meeting/:id" });
+  };
+  function renderEventContent(eventInfo: {
+    event: { title: string; start: Date; end?: Date; backgroundColor?: string };
+  }) {
+    const { title, backgroundColor } = eventInfo.event;
 
     return (
       <div
@@ -74,10 +46,6 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({
         className="event flex flex-col h-[100%] w-full cursor-pointer"
         style={{ backgroundColor }}
       >
-        {/* <span>{startDate} {startTime}</span>
-        {displayEndDate && <span>{displayEndDate} {displayEndTime}</span>}
-        {!displayEndDate && displayEndTime && <span>{displayEndTime}</span>}
-        {duration && <span>{duration}</span>} */}
         <span>{title}</span>
       </div>
     );
