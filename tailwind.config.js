2c4fffde65175ab9b7544816d4a416fb357bf665
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-06-09 13:16:44 +0700

    add material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
 /** @type {import('tailwindcss').Config} */
-export default {
+
+const config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
 };
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+export default withMT(config);
+
 
